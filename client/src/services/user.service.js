a2cb50489e52fed5919c6fb74d99c1344bb52c51
Pam Kelly <plkelly@yahoo.com>
2020-05-09 09:50:19 -0700

    Reverted.

--- client/src/services/user.service.js
+++ client/src/services/user.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import authHeader from './auth-header';
 
-const API_URL = "https://auth-unlockit.herokuapp.com/api/auth/";
+const API_URL = "https://auth-unlockit.herokuapp.com/api/test/";
 
 class UserService {
   getPublicContent() {
